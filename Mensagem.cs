06c37700aa4e4877116e19360cdc5374e98e095f
Kaue Portella <portellakaue@gmail.com>
2021-07-05 10:20:05 -0300

    First Commit

---
+++ Mensagem.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace pooMensagem
+{
+    public class Mensagem
+    {
+        public string TextoMensagem;
+
+        public void exibirMensagem()
+        {
+            Console.WriteLine(TextoMensagem);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pooMensagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem mensagem1, mensagem2;
+            mensagem1 = new Mensagem();
+            mensagem1.TextoMensagem = "Olá Mundo";
+            mensagem1.exibirMensagem();
+
+
+            mensagem2 = new Mensagem();
+            mensagem2.TextoMensagem = "Segundo Objeto";
+            mensagem2.exibirMensagem();
+            Console.ReadKey();
+
+        }
+    }
+}
