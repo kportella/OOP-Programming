a158553c182dd6828a3cedded337eafe19931c5b
Kaue Portella <portellakaue@gmail.com>
2021-07-05 10:43:15 -0300

    Propriedades no C#

--- Mensagem.cs
+++ Mensagem.cs
@@ -4,21 +4,22 @@ namespace pooMensagem
 {
     public class Mensagem
     {
-        private string TextoMensagem;
+        private String textoMensagem;
+        public String TextoMensagem
+        {
+            get
+            {
+                return this.textoMensagem;
+            }
+            set
+            {
+                this.textoMensagem = value.ToUpper();
+            }
+        }
 
         public void exibirMensagem()
         {
             Console.WriteLine(this.TextoMensagem);
-        }
-
-        public String getTextoMensagem()
-        {
-            return this.TextoMensagem;
-        }
-
-        public void setTextoMensagem(String valor)
-        {
-            this.TextoMensagem = valor;
         }
     }
 }
